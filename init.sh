db48f0e7891c19fa132828f73440906800f822e6
tomoyuki-tanaka <masuminium5@gmail.com>
2017-02-08 23:48:21 +0900

    add fonts setup command

--- init.sh
+++ init.sh
@@ -4,3 +4,5 @@ ln -s dotfiles/.vimrc ~/.vimrc
 ln -s dotfiles/.agignore ~/.agignore
 ln -s dotfiles/.vim/plugin.toml ~/.vim/plugin.toml
 ln -s dotfiles/.vim/lightline.conf.vim ~/.vim/lightline.conf.vim
+cp dotfiles/fonts/*.ttf ~/Library/Fonts/
+fc-cache -vf
