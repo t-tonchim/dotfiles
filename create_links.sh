2562790d469b4d068da842678c456399f2187845
tomoyuki-tanaka <masuminium5@gmail.com>
2017-02-08 23:35:17 +0900

    edit create_links.sh

--- create_links.sh
+++ create_links.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 
-ln -s dotfiles/.vim ~/.vim
 ln -s dotfiles/.vimrc ~/.vimrc
 ln -s dotfiles/.agignore ~/.agignore
+ln -s dotfiles/.vim/plugin.toml ~/.vim/plugin.toml
+ln -s dotfiles/.vim/lightline.conf.vim ~/.vim/lightline.conf.vim
