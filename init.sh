340aa3b6431a43d621afc9effbcbb772052b8f6e
tomoyuki.tanaka.a <tomoyuki.tanaka.a@enigmo.co.jp>
2017-02-09 09:32:27 +0900

    fix init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,13 @@
 ln -s dotfiles/.vimrc ~/.vimrc
 ln -s dotfiles/.agignore ~/.agignore
 ln -s dotfiles/.vim/plugin.toml ~/.vim/plugin.toml
-ln -s dotfiles/.vim/lightline.conf.vim ~/.vim/lightline.conf.vim
-cp dotfiles/fonts/*.ttf ~/Library/Fonts/
+cp dotfiles/.vim/lightline.conf.vim ~/.vim/lightline.conf.vim
+cp dotfiles/fonts/Ricty-Bold-Powerline.ttf ~/Library/Fonts/
+cp dotfiles/fonts/Ricty-Bold.ttf ~/Library/Fonts/
+cp dotfiles/fonts/Ricty-Regular-Powerline.ttf ~/Library/Fonts/
+cp dotfiles/fonts/Ricty-Regular.ttf ~/Library/Fonts/
+cp dotfiles/fonts/RictyDiscord-Bold-Powerline.ttf ~/Library/Fonts/
+cp dotfiles/fonts/RictyDiscord-Bold.ttf ~/Library/Fonts/
+cp dotfiles/fonts/RictyDiscord-Regular-Powerline.ttf ~/Library/Fonts/
+cp dotfiles/fonts/RictyDiscord-Regular.ttf ~/Library/Fonts/
 fc-cache -vf
