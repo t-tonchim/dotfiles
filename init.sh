75b6e7e31452ce448c76e29fcd8c8e32d46db2cd
tomoyuki.tanaka.a <tomoyuki.tanaka.a@enigmo.co.jp>
2017-07-10 12:55:26 +0900

    add js to ctags

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 
 ln -s dotfiles/.vimrc ~/.vimrc
+ln -s dotfiles/.ctags ~/.ctags
 ln -s dotfiles/.agignore ~/.agignore
 ln -s dotfiles/.vim/plugin.toml ~/.vim/plugin.toml
 ln -s dotfiles/.neocompleteconf.vim ~/.neocompleteconf.vim
